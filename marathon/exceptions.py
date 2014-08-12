2a6a2afd2d9e5e63efcfae79bfdaa6e92c6c5c60
James McGuinness <jrm@yelp.com>
2014-08-12 11:39:03 -0700

    making MarathonHttpError parse response correctly

--- marathon/exceptions.py
+++ marathon/exceptions.py
@@ -10,13 +10,12 @@ class MarathonHttpError(MarathonError):
         """
         content = response.json()
         self.status_code = response.status_code
-        self.status_desc = content['error']['statusdesc']
-        self.error_message  = content['error']['errormessage']
+        self.error_message  = content['message']
         super(MarathonHttpError, self).__init__(self.__str__() )
 
     def __repr__(self):
-        return 'MarathonHttpError: HTTP `%s - %s` returned with message, "%s"' % \
-               (self.status_code, self.status_desc, self.error_message)
+        return 'MarathonHttpError: HTTP %s returned with message, "%s"' % \
+               (self.status_code, self.error_message)
 
     def __str__(self):
         return self.__repr__()
