5b7c7f5d16929eed3ae7e6994c2aed52dc9c1330
Kyle Anderson <kwa@yelp.com>
2015-07-10 09:20:53 -0700

    Added a matrix of marathon versions to test against

--- itests/install-marathon.sh
+++ itests/install-marathon.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -vx
 
+# Default version of marathon to test against if not
+# set already by travis
+MARATHONVERSION="${MARATHONVERSION:-0.8.2}"
+
 sudo apt-get update -q
 
 # Setup
@@ -14,4 +18,4 @@ echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" |
 sudo apt-get -y update
 
 # Install packages
-sudo apt-get -y --force-yes install mesos marathon
+sudo apt-get -y --force-yes install mesos marathon=$MARATHONVERSION*
