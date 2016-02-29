44d411b282e7aa770fb7ea7711ae72365bbdfc16
Kyle Anderson <kyle@xkyle.com>
2016-02-29 07:52:04 -0800

    Release 0.7.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info >= (3,):
 
 setup(
     name='marathon',
-    version='0.7.6',
+    version='0.7.7',
     description='Marathon Client Library',
     long_description="""Python interface to the Mesos Marathon REST API.""",
     author='Mike Babineau',
