efc8802f4fd2f7b697aab2936afd91dc25cd0f4e
Anatolii Lapytskyi <ala@allunite.com>
2016-04-17 13:07:41 +0300

    Fix test

--- itests/steps/marathon_steps.py
+++ itests/steps/marathon_steps.py
@@ -154,4 +154,4 @@ def stop_listening_stream(context):
 
     # and 2 deployment_step_success events
     filtered_events = [e for e in context.events if e.event_type == "deployment_success"]
-    assert len(filtered_events) == 1
+    assert len(filtered_events) == 2
