b7ed965075dc955a3f0fa292d66f006434a182ba
Nathan Handler <nhandler@yelp.com>
2017-02-21 10:37:09 -0800

    Marathon 1.4.0 now requires mesos 1.1.0

--- itests/install-marathon.sh
+++ itests/install-marathon.sh
@@ -20,7 +20,7 @@ sudo apt-get -y purge oracle-java7-installer
 sudo update-java-alternatives -s java-8-oracle
 sudo DEBIAN_FRONTEND=noninteractive apt-get install oracle-java8-set-default
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install mesos=1.0.* marathon=$MARATHONVERSION*
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install mesos=1.1.* marathon=$MARATHONVERSION*
 
 # WTF MARATHON?
 # Why does the precise version have java7 hardcoded if it requires java8?
