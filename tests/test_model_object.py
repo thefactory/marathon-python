0519824c537a96474e0501e1ac45f7a626391a31
Joseph Lynch <joe.e.lynch@gmail.com>
2017-06-26 22:08:13 -0700

    Add regression test for MarathonResource

--- tests/test_model_object.py
+++ tests/test_model_object.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 from marathon.models.base import MarathonObject
+from marathon.models.base import MarathonResource
 import unittest
 
 
@@ -16,6 +17,24 @@ class MarathonObjectTest(unittest.TestCase):
         This test ensures that we are hashable in all versions of python
         """
         obj = MarathonObject()
+        collection = {}
+        collection[obj] = True
+        assert collection[obj]
+
+
+class MarathonResourceHashable(unittest.TestCase):
+
+    def test_hashable(self):
+        """
+        Regression test for issue #203
+
+        MarathonResource defined __eq__ but not __hash__, meaning that in
+        in Python2.7 MarathonResources are hashable, but in Python3 they're
+        not
+
+        This test ensures that we are hashable in all versions of python
+        """
+        obj = MarathonResource()
         collection = {}
         collection[obj] = True
         assert collection[obj]
