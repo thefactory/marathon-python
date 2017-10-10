2cdc850fc667904b2e4680c2c55459e12ac9a8ae
Dmitriy Samovskiy <dmits@yelp.com>
2017-10-10 14:34:01 -0700

    support more datetime formats in MarathonAppVersionInfo

--- tests/test_model_app.py
+++ tests/test_model_app.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
-from marathon.models.app import MarathonApp
+from marathon.models.app import MarathonApp, MarathonAppVersionInfo
+from datetime import datetime
 import unittest
 
 
@@ -24,3 +25,10 @@ class MarathonAppTest(unittest.TestCase):
 
         app.add_env("MY_ENV", "my-value")
         self.assertDictEqual({"MY_ENV": "my-value", "OTHER_ENV": "other-value"}, app.env)
+
+    def test_version_info_datetime(self):
+        app_ver_info = MarathonAppVersionInfo()
+        self.assertEquals(app_ver_info._to_datetime("2017-09-28T00:31:55Z"), datetime(2017, 9, 28, 0, 31, 55))
+        self.assertEquals(app_ver_info._to_datetime("2017-09-28T00:31:55.4Z"), datetime(2017, 9, 28, 0, 31, 55, 400000))
+        self.assertEquals(app_ver_info._to_datetime("2017-09-28T00:31:55.004Z"), datetime(2017, 9, 28, 0, 31, 55, 4000))
+        self.assertEquals(app_ver_info._to_datetime("2017-09-28T00:31:55.00042Z"), datetime(2017, 9, 28, 0, 31, 55, 420))
