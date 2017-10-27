262c659967d3c2b6f82d3e470c877a905a5518db
diogommartins <magalhaesmartins@icloud.com>
2017-10-27 17:25:27 -0200

    Adding MarathonConstraint.from_string classmethod tests

--- tests/test_model_constraint.py
+++ tests/test_model_constraint.py
@@ -33,3 +33,22 @@ class MarathonConstraintTests(unittest.TestCase):
     def test_from_json_without_value(self):
         constraint = MarathonConstraint.from_json(['a_field', 'OPERATOR'])
         self.assertEqual(constraint, MarathonConstraint('a_field', 'OPERATOR'))
+
+    def test_from_string_with_value(self):
+        constraint = MarathonConstraint.from_string('a_field:OPERATOR:a_value')
+        self.assertEqual(constraint,
+                         MarathonConstraint('a_field', 'OPERATOR', 'a_value'))
+
+    def test_from_string_without_value(self):
+        constraint = MarathonConstraint.from_string('a_field:OPERATOR')
+        self.assertEqual(constraint, MarathonConstraint('a_field', 'OPERATOR'))
+
+    def test_from_string_raises_an_error_for_invalid_format(self):
+        with self.assertRaises(ValueError):
+            MarathonConstraint.from_string('a_field:OPERATOR:a_value:')
+
+        with self.assertRaises(ValueError):
+            MarathonConstraint.from_string('a_field')
+
+        with self.assertRaises(ValueError):
+            MarathonConstraint.from_string('a_field:OPERATOR:a_value:something')
